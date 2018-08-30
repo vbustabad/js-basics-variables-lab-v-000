561ca19c97b2826720960a722d28e7cccf9b911a
Veronica Bustabad <vbustabad@gmail.com>
2018-08-30 23:26:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const companyName = 'Scuber';
 const companyName = 'specialCompany';
 let mostProfitableNeighborhood = 'Chelsea';
 let mostProfitableNeighborhood = 'Upper West Side';
+var companyCeo = 'Susan Smith';
 var companyCeo = 'Lauren Hart';
