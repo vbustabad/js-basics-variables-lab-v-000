a0ebaed03d7b2e241caef5bb5b01b3eaf877f470
Veronica Bustabad <vbustabad@gmail.com>
2018-08-30 23:28:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+const companyName = 'Scuber';
+// const companyName = 'specialCompany';
+let mostProfitableNeighborhood = 'Chelsea';
+// let mostProfitableNeighborhood = 'Upper West Side';
+var companyCeo = 'Susan Smith';
+// var companyCeo = 'Lauren Hart';
